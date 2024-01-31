a0ad11618815b7b3e8cf2abdfcd43891e92f8209
andrewzhang <azhang26@uw.edu>
2024-01-31 00:15:19 -0800

    add divi component

---
+++ packages/components/src/divi-viz.js
@@ -0,0 +1,52 @@
+import { ArticleElement } from '@living-papers/components';
+import { ascending } from 'd3-array'
+// import { hydrate } from 'uwdata-divi';
+// import { Color } from 'd3-color'
+// import { hydrate } from '/node_modules/uwdata-divi';
+
+export default class Divi extends ArticleElement {
+
+  static get properties() {
+    return {
+    //filepath of svg file (i.e. "figures/impulse_1d.svg")
+      filepath: {type: String},
+    };
+  }
+
+  constructor() {
+    super();
+    this.filepath = "";
+  }
+
+
+  render() {
+    //create a div and display this svg
+    this._root = document.createElement('div');
+    const svgCode = '<svg <rect width="10" height="10" y="80"></rect> </svg>';
+    const svgContainer = document.createElement('div');
+    svgContainer.innerHTML = svgCode;
+    console.log(ascending(1, 2));
+    // hydrate(null);
+    // hydrate(svgContainer)
+    // Color();
+    // this._root.innerHTML = `<img src="${this.svg}"/>`
+    // let svg = fetch(this.filepath)
+    //   .then(response => response.text())
+    //   .catch(error => console.log(error))
+    
+    // this.svg = svg.then(result => console.log(result))
+    // console.log("test1  " + this.svg)
+    // console.log(this.svg)
+    // this.svg = fetch(this.filepath)
+    //   .then(response => response.text())
+    //   .catch(text => console.log(text))
+
+    // fetch(this.filepath)
+    //   .then(response => response.text())
+    //   .then(text => console.log(hydrate(text)))
+    
+    this._root.innerHTML = this.filepath;
+    return this._root
+  }
+}
+
